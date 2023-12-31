082beb37c7f4008a984bea9a8858fa9a22f4729f
marco <marcoaureliotomaz@gmail.com>
2023-12-31 00:34:41 -0300

    Conexao com mySQL remoto. Criação da Tabela Persistência Popular combo

--- src/main/java/com/gryphem/MyWallet/controller/TituloController.java
+++ src/main/java/com/gryphem/MyWallet/controller/TituloController.java
@@ -1,12 +1,17 @@
 package com.gryphem.MyWallet.controller;
 
+import com.gryphem.MyWallet.model.StatusTitulo;
 import com.gryphem.MyWallet.model.Titulo;
 import com.gryphem.MyWallet.repository.Titulos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/titulos")
@@ -15,8 +20,10 @@ public class TituloController {
     @Autowired
     private Titulos titulos;
     @RequestMapping("/novo")
-    public String novo(){
-        return "CadastroTitulo";
+    public ModelAndView novo(){
+        ModelAndView mv = new ModelAndView("CadastroTitulo");
+        mv.addObject("todosStatusTitulo", StatusTitulo.values());
+        return mv;
     }
 
     @RequestMapping(method = RequestMethod.POST)
@@ -27,5 +34,10 @@ public class TituloController {
         mv.addObject("mensagem","Tìtulo salvo com sucesso!");
         return mv;
 
+    }
+
+    @ModelAttribute("todosStatusTitulo")
+    public List<StatusTitulo> todosStatusTitulo(){
+        return Arrays.asList(StatusTitulo.values());
     }
 }
--- src/main/java/com/gryphem/MyWallet/model/StatusTitulo.java
+++ src/main/java/com/gryphem/MyWallet/model/StatusTitulo.java
@@ -2,6 +2,7 @@ package com.gryphem.MyWallet.model;
 
 public enum StatusTitulo {
     PENDENTE("Pendente"),
+    //CANCELADO("Cancelado"),
     RECEBIDO("Recebido");
 
     private String descricao;
