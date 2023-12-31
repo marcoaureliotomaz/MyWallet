02df125dac566670611e8b84f2c23e2b263bcde8
marco <marcoaureliotomaz@gmail.com>
2023-12-31 01:01:26 -0300

    Tela de Pesquisa de titulos iniciada

--- src/main/java/com/gryphem/MyWallet/controller/TituloController.java
+++ src/main/java/com/gryphem/MyWallet/controller/TituloController.java
@@ -36,6 +36,11 @@ public class TituloController {
 
     }
 
+    @RequestMapping
+    public String pesquisar(){
+        return "PesquisaTitulos";
+    }
+
     @ModelAttribute("todosStatusTitulo")
     public List<StatusTitulo> todosStatusTitulo(){
         return Arrays.asList(StatusTitulo.values());
