068e702658207b4a9cbf231c2f01fd2318999b46
marco <marcoaureliotomaz@gmail.com>
2024-01-03 18:21:53 -0300

    Edição de Títulos

--- src/main/java/com/gryphem/MyWallet/controller/TituloController.java
+++ src/main/java/com/gryphem/MyWallet/controller/TituloController.java
@@ -8,55 +8,68 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
 
+    private static final String CADASTRO_VIEW= "CadastroTitulo";
+
     @Autowired
     private Titulos titulos;
+
+
+
+
+
+
     @RequestMapping("/novo")
-    public ModelAndView novo(){
-        ModelAndView mv = new ModelAndView("CadastroTitulo");
+    public ModelAndView novo() {
+        ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
         mv.addObject(new Titulo());
         return mv;
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String salvar(@Validated   Titulo titulo, Errors errors, RedirectAttributes redirectAttributes) {
-        ModelAndView mv = new ModelAndView("CadastroTitulo");
-        if (errors.hasErrors()){
-
-            return "CadastroTitulo";
+    public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes redirectAttributes) {
+        ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+        if (errors.hasErrors()) {
+            return CADASTRO_VIEW;
         }
-
         titulos.save(titulo);
-
-        redirectAttributes.addFlashAttribute("mensagem","Tìtulo salvo com sucesso!");
+        redirectAttributes.addFlashAttribute("mensagem", "Tìtulo salvo com sucesso!");
         return "redirect:/titulos/novo";
-
     }
 
     @RequestMapping
-    public ModelAndView pesquisar(){
-
+    public ModelAndView pesquisar() {
         List<Titulo> todosTitulos = titulos.findAll();
         ModelAndView mv = new ModelAndView("PesquisaTitulos");
         mv.addObject("titulos", todosTitulos);
-
         return mv;
     }
 
     @ModelAttribute("todosStatusTitulo")
-    public List<StatusTitulo> todosStatusTitulo(){
+    public List<StatusTitulo> todosStatusTitulo() {
         return Arrays.asList(StatusTitulo.values());
+    }
+
+
+    @RequestMapping("{codigo}")
+    public ModelAndView edicao(@PathVariable Long codigo){
+        Titulo titulo = titulos.findById(codigo).orElse(new Titulo());
+
+
+        ModelAndView mv =  new ModelAndView(CADASTRO_VIEW);
+
+        mv.addObject(titulo);
+        return mv;
     }
 }
